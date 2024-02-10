b30cc9107ad2294604b14588f2c46a180d4a7f38
WALM <78268817+evembar@users.noreply.github.com>
2024-02-11 00:38:57 +0400

    Add files via upload

---
+++ support/code0.js
@@ -0,0 +1,281 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.GDwalmObjects1= [];
+gdjs.Untitled_32sceneCode.GDwalmObjects2= [];
+gdjs.Untitled_32sceneCode.GDwalmObjects3= [];
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects1= [];
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects2= [];
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects3= [];
+gdjs.Untitled_32sceneCode.GDSupportObjects1= [];
+gdjs.Untitled_32sceneCode.GDSupportObjects2= [];
+gdjs.Untitled_32sceneCode.GDSupportObjects3= [];
+gdjs.Untitled_32sceneCode.GDmaksimysObjects1= [];
+gdjs.Untitled_32sceneCode.GDmaksimysObjects2= [];
+gdjs.Untitled_32sceneCode.GDmaksimysObjects3= [];
+gdjs.Untitled_32sceneCode.GDnameObjects1= [];
+gdjs.Untitled_32sceneCode.GDnameObjects2= [];
+gdjs.Untitled_32sceneCode.GDnameObjects3= [];
+gdjs.Untitled_32sceneCode.GDname2Objects1= [];
+gdjs.Untitled_32sceneCode.GDname2Objects2= [];
+gdjs.Untitled_32sceneCode.GDname2Objects3= [];
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects1= [];
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects2= [];
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects3= [];
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects1= [];
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects2= [];
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects3= [];
+gdjs.Untitled_32sceneCode.GDvkObjects1= [];
+gdjs.Untitled_32sceneCode.GDvkObjects2= [];
+gdjs.Untitled_32sceneCode.GDvkObjects3= [];
+gdjs.Untitled_32sceneCode.GDvk2Objects1= [];
+gdjs.Untitled_32sceneCode.GDvk2Objects2= [];
+gdjs.Untitled_32sceneCode.GDvk2Objects3= [];
+gdjs.Untitled_32sceneCode.GDtelegramObjects1= [];
+gdjs.Untitled_32sceneCode.GDtelegramObjects2= [];
+gdjs.Untitled_32sceneCode.GDtelegramObjects3= [];
+gdjs.Untitled_32sceneCode.GDtelegram2Objects1= [];
+gdjs.Untitled_32sceneCode.GDtelegram2Objects2= [];
+gdjs.Untitled_32sceneCode.GDtelegram2Objects3= [];
+gdjs.Untitled_32sceneCode.GDxObjects1= [];
+gdjs.Untitled_32sceneCode.GDxObjects2= [];
+gdjs.Untitled_32sceneCode.GDxObjects3= [];
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects1= [];
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects2= [];
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects3= [];
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects1= [];
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects2= [];
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects3= [];
+gdjs.Untitled_32sceneCode.GDgithubObjects1= [];
+gdjs.Untitled_32sceneCode.GDgithubObjects2= [];
+gdjs.Untitled_32sceneCode.GDgithubObjects3= [];
+
+
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDdonationalertsObjects2Objects = Hashtable.newFrom({"donationalerts": gdjs.Untitled_32sceneCode.GDdonationalertsObjects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoomoneyObjects2Objects = Hashtable.newFrom({"yoomoney": gdjs.Untitled_32sceneCode.GDyoomoneyObjects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDvkObjects2Objects = Hashtable.newFrom({"vk": gdjs.Untitled_32sceneCode.GDvkObjects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDvk2Objects2Objects = Hashtable.newFrom({"vk2": gdjs.Untitled_32sceneCode.GDvk2Objects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDtelegramObjects2Objects = Hashtable.newFrom({"telegram": gdjs.Untitled_32sceneCode.GDtelegramObjects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDtelegram2Objects2Objects = Hashtable.newFrom({"telegram2": gdjs.Untitled_32sceneCode.GDtelegram2Objects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoutube_95959595Objects2Objects = Hashtable.newFrom({"youtube_": gdjs.Untitled_32sceneCode.GDyoutube_9595Objects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoutube_959595952Objects2Objects = Hashtable.newFrom({"youtube_2": gdjs.Untitled_32sceneCode.GDyoutube_95952Objects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDxObjects2Objects = Hashtable.newFrom({"x": gdjs.Untitled_32sceneCode.GDxObjects2});
+gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDgithubObjects1Objects = Hashtable.newFrom({"github": gdjs.Untitled_32sceneCode.GDgithubObjects1});
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("donationalerts"), gdjs.Untitled_32sceneCode.GDdonationalertsObjects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDdonationalertsObjects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://www.donationalerts.com/r/webanlimaks", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("yoomoney"), gdjs.Untitled_32sceneCode.GDyoomoneyObjects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoomoneyObjects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://yoomoney.ru/to/4100118376371851", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("vk"), gdjs.Untitled_32sceneCode.GDvkObjects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDvkObjects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://vk.com/webmast1", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("vk2"), gdjs.Untitled_32sceneCode.GDvk2Objects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDvk2Objects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://vk.com/maksstadnik", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("telegram"), gdjs.Untitled_32sceneCode.GDtelegramObjects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDtelegramObjects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://t.me/webmast_webanlimaks", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("telegram2"), gdjs.Untitled_32sceneCode.GDtelegram2Objects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDtelegram2Objects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://t.me/Original_Maksimys", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("youtube_"), gdjs.Untitled_32sceneCode.GDyoutube_9595Objects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoutube_95959595Objects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://www.youtube.com/@webanlimaks", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("youtube_2"), gdjs.Untitled_32sceneCode.GDyoutube_95952Objects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDyoutube_959595952Objects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://www.youtube.com/@Maksimys_", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("x"), gdjs.Untitled_32sceneCode.GDxObjects2);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDxObjects2Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://twitter.com/Origin_Maksimys", runtimeScene);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("github"), gdjs.Untitled_32sceneCode.GDgithubObjects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs.Untitled_32sceneCode.mapOfGDgdjs_9546Untitled_959532sceneCode_9546GDgithubObjects1Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://github.com/evembar", runtimeScene);
+}}
+
+}
+
+
+};gdjs.Untitled_32sceneCode.eventsList1 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.hasAnyTouchOrMouseStarted(runtimeScene);
+if (isConditionTrue_0) {
+
+{ //Subevents
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDwalmObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDwalmObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDwalmObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDwebmast_9595walmObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDSupportObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDSupportObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDSupportObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDmaksimysObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDmaksimysObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDmaksimysObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDnameObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDnameObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDnameObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDname2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDname2Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDname2Objects3.length = 0;
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDdonationalertsObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDyoomoneyObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDvkObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDvkObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDvkObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDvk2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDvk2Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDvk2Objects3.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegramObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegramObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegramObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegram2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegram2Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDtelegram2Objects3.length = 0;
+gdjs.Untitled_32sceneCode.GDxObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDxObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDxObjects3.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_9595Objects3.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDyoutube_95952Objects3.length = 0;
+gdjs.Untitled_32sceneCode.GDgithubObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDgithubObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDgithubObjects3.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList1(runtimeScene);
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
